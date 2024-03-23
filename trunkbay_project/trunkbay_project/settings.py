a87f3b2052cb1f6cf7b3252ca692ed11dc6b4db7
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-03-23 22:58:59 +0100

    new: added header and footer html templates from bootstrap

--- trunkbay_project/trunkbay_project/settings.py
+++ trunkbay_project/trunkbay_project/settings.py
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'static/trunkbay_project',
+    # BASE_DIR / 'static/trunkbay_project',
 ]
